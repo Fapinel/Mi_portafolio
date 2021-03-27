bec7aea50192bb71e39af1f5f43d0f762f377102
Fapinel <faratpinel@gmail.com>
2021-03-26 21:38:14 -0600

    seventh commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
     $(window).scroll(function(){
         // sticky navbar on scroll script
@@ -10,9 +12,39 @@ $(document).ready(function(){
 
 
 
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
 // typing animation script
 var typed = new Typed(".typing", {
-	strings: [ "Web Developer", "Software Developer", "Front-End", "Web Application Security"],
+	strings: [ "React Developer", "Front-End", "Web Application Security"],
 	typeSpeed: 100,
 	backSpeed: 60,
 	loop: true
